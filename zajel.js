05ad0874ba11865a118dd4b3b5d5e73eba254b1e
noha-isaed <thahernoha@gmail.com>
2020-05-02 03:31:00 +0300

    initial commit

--- zajel.js
+++ zajel.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const calenderRouter = require('./routes/calender');
 const app = express();
+
 mongoose.connect('mongodb://localhost/zajel' , {useNewUrlParser: true ,  useUnifiedTopology: true });
 
 app.set('view engine' , 'ejs');
 app.use(express.urlencoded({extended : false})); 
 app.use(express.static("public"));
 
+app.use( '/events', calenderRouter);
+
 app.get("/" , function(req , res){
-   res.render('index');
+   res.render('events/index');
 })
 
-app.post("/" , function(req , res){
-    
-})
 
 app.listen(8000, function(){
     console.log("Server started in port 8000");
 })
+
+
+  
